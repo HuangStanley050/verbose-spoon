ecd43a287908196f9f1a177cff7709be6a7ea97a
Stanley Huang <infamous_godhand@yahoo.com>
2020-05-20 12:26:10 +1000

    basic server set up

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const Index = () => (
+  <div>
+    <h2 className="title is-2">Authentication with Next.js and JWT</h2>
+    <h3>hi there</h3>
+    <p>
+      A proof of concept app, demonstrating the authentication of Next.js
+      application using JWT.
+    </p>
+  </div>
+);
+
+export default Index;

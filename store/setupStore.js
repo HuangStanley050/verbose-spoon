31b6add009c9cde865dc7c6bae20737f2a6f5a09
Stanley Huang <infamous_godhand@yahoo.com>
2020-05-28 20:22:03 +1000

    cookie get weird %22 after getting token

--- store/setupStore.js
+++ store/setupStore.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createWrapper, HYDRATE } from "next-redux-wrapper";
 import { composeWithDevTools } from "redux-devtools-extension";
+import reduxCookiesMiddleware from "redux-cookies-middleware";
+import getStateFromCookies from "redux-cookies-middleware/lib/getStateFromCookies";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
+import { setCookie } from "../util/cookieHelper";
 
 const reducer = (state, action) => {
   if (action.type === HYDRATE) {
@@ -16,12 +19,24 @@ const reducer = (state, action) => {
     return rootReducer(state, action);
   }
 };
+let initialState = {
+  auth: {
+    token: null,
+  },
+};
 
-const makeStore = ({ foo, auth }) => {
+// state to persist in cookies
+const paths = {
+  "auth.token": { name: "my_app_token" },
+};
+initialState = getStateFromCookies(initialState, paths);
+const makeStore = ({ foo, auth, session }) => {
   return createStore(
     reducer,
-
-    composeWithDevTools(applyMiddleware(thunk))
+    initialState,
+    composeWithDevTools(
+      applyMiddleware(thunk, reduxCookiesMiddleware(paths, { setCookie }))
+    )
   );
 };
 

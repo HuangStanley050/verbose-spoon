4da6f40b75acef873e53107312471422b15bb0ce
Stanley Huang <infamous_godhand@yahoo.com>
2020-05-29 20:41:52 +1000

    persisted cookie get re dispatch to store

--- store/reducers/fooReducer.js
+++ store/reducers/fooReducer.js
@@ -2,6 +2,12 @@ import * as actionTypes from "../actions/actionTypes";
 //import { HYDRATE } from "next-redux-wrapper";
 const reducer = (state = { foo: "", count: 0 }, action) => {
   switch (action.type) {
+    case actionTypes.REAPPLY:
+      //console.log(Number(action.payload));
+      return {
+        ...state,
+        count: parseInt(action.payload),
+      };
     case actionTypes.MINUS:
       return {
         ...state,
